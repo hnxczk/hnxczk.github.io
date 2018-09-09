5b16cd996dccaddb05f0d884168be262847a9cd1
zhouke <hnxczk@126.com>
2018-09-09 22:33:55 +0800

    提交

---
+++ articles/reading-notes/图说设计模式/code/02-factory-method/02-factory-method/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  02-factory-method
+//
+//  Created by zhouke on 2018/9/9.
+//  Copyright © 2018年 zhouke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var factory: AbstractFactory?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func leftClick() {
+        self.factory = FactoryA()
+        self.pushWithFactory()
+    }
+    
+    @IBAction func rightClick() {
+        self.factory = FactoryB()
+        self.pushWithFactory()
+    }
+    
+    func pushWithFactory() {
+        let vc = self.factory?.creatController()
+        if let vc = vc {
+            self.present(vc, animated: true, completion: nil)
+        }
+    }
+}
+
