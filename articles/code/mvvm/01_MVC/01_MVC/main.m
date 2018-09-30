a3c556ac2e4d85ab4963082154b0e9f0505dc098
zhouke <zhouke@mingtian.com>
2018-09-30 15:25:00 +0800

    添加MVVM实战

---
+++ articles/code/mvvm/01_MVC/01_MVC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  01_MVC
+//
+//  Created by zhouke on 2018/9/30.
+//  Copyright © 2018 zhouke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
