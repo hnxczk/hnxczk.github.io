293e102080a520b38c1347c2a43c8834337f4947
hnxczk <hnxczk@126.com>
2019-07-24 15:23:19 +0800

    添加博客界面

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:hnxczk/hnxczk.github.io.git master
+git push -f git@github.com:hnxczk/blog.git master:gh-pages
 
 cd -
